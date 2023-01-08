d802414912a0c219b194e8db74b89e81f0469790
oldmanclancy <wldfngrs@gmail.com>
2023-01-08 21:41:42 +0100

    implementation of Pratt's parsing technique for executing statements

--- Olive-bci/compiler.c
+++ Olive-bci/compiler.c
@@ -1,23 +1,251 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "chunk.h"
 
-void compile(const char* source) {
-	initScanner(source);
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
+
+typedef struct {
+	Token current;
+	Token previous;
+	bool hadError;
+	bool panicMode;
+} Parser;
+
+typedef enum {
+	PREC_NONE,
+	PREC_ASSIGNMENT,
+	PREC_OR,
+	PREC_AND,
+	PREC_EQUALITY,
+	PREC_COMPARISON,
+	PREC_TERM,
+	PREC_FACTOR,
+	PREC_UNARY,
+	PREC_CALL,
+	PREC_PRIMARY
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+	ParseFn prefix;
+	ParseFn infix;
+	Precedence precedence;
+} ParseRule;
+
+Parser parser;
+
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+	return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message) {
+	if (parser.panicMode) return;
+	parser.panicMode = true;
+
+	fprintf(stderr, "[line %d] Error", token->line);
 	
-	int line = -1;
-	for (;;) {
-		Token token = scanToken();
-		if (token.line != line) {
-			printf("%4d ", token.line);
-			line = token.line;
-		} else {
-			printf("   | ");
-		}
-		printf("%2d '%.*s'\n", token.type, token.length, token.start);
-		
-		if (token.type == TOKEN_EOF) break;
+	if (token->type == TOKEN_EOF) {
+		fprintf(stderr, " at end");
+	} else if (token->type == TOKEN_ERROR) {
+		// nothing
+	} else {
+		fprintf(stderr, " at '%.*s'", token->length, token->start);
 	}
+	
+	fprintf(stderr, ": %s\n", message);
+	parser.hadError = true;
+}
+
+static void error(const char* message) {
+	errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message) {
+	errorAt(&parser.current, message);
+}
+
+static void advance() {
+	parser.previous = parser.current;
+	
+	for (;;) {
+		parser.current = scanToken();
+		if(parser.current.type != TOKEN_ERROR) break;
+		
+		errorAtCurrent(parser.current.start);
+	}
+}
+
+static void consume(TokenType type, const char* message) {
+	if (parser.current.type == type) {
+		advance();
+		return;
+	}
+	
+	errorAtCurrent(message);
+}
+
+
+static void emitByte(uint8_t byte) {
+	writeChunk(currentChunk(), byte, parser.previous.line);
+}
+/*
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+	emitByte(byte1);
+	emitByte(byte2);
+}*/
+
+static void emitReturn() {
+	emitByte(OP_RETURN);
+}
+/*
+static uint8_t makeConstant(Value value) {
+	int constant = addConstant(currentChunk(), value);
+	return (uint8_t)constant;
+}*/
+
+static void emitConstant(Value value) {
+	writeConstant(currentChunk(), value, parser.previous.line);
+}
+
+static void endCompiler() {
+	emitReturn();
+#ifdef DEBUG_PRINT_CODE
+	if(!parser.hadError) {
+		disassembleChunk(currentChunk(), "code");
+	}
+#endif
+}
+
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary() {
+	TokenType operatorType = parser.previous.type;
+	
+	ParseRule* rule = getRule(operatorType);
+	// check why '+ 1'
+	parsePrecedence((Precedence)(rule->precedence + 1));
+	
+	switch(operatorType) {
+		case TOKEN_PLUS: emitByte(OP_ADD); break;
+		case TOKEN_MINUS: emitByte(OP_SUBTRACT); break;
+		case TOKEN_STAR: emitByte(OP_MULTIPLY); break;
+		case TOKEN_SLASH: emitByte(OP_DIVIDE); break;
+		default:
+			return;
+	}
+}
+
+
+static void grouping() {
+	expression();
+	consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number() {
+	double value = strtod(parser.previous.start, NULL);
+	emitConstant(value);
+}
+
+static void unary() {
+	TokenType operatorType = parser.previous.type;
+	
+	parsePrecedence(PREC_UNARY);
+	
+	switch(operatorType) {
+		case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+		default:
+			return;
+	}
+}
+
+ParseRule rules[] = {
+	[TOKEN_LEFT_PAREN]= {grouping,NULL,PREC_NONE},
+	[TOKEN_RIGHT_PAREN]= {NULL,NULL,PREC_NONE},
+	[TOKEN_LEFT_BRACE]= {NULL,NULL,PREC_NONE},
+	[TOKEN_RIGHT_BRACE]= {NULL,NULL,PREC_NONE},
+	[TOKEN_COMMA]= {NULL,NULL,PREC_NONE},
+	[TOKEN_DOT]= {NULL,NULL,PREC_NONE},
+	[TOKEN_MINUS]= {unary,binary,PREC_TERM},
+	[TOKEN_PLUS]= {NULL,binary,PREC_TERM},
+	[TOKEN_SEMICOLON]= {NULL,NULL,PREC_NONE},
+	[TOKEN_SLASH]= {NULL,binary,PREC_FACTOR},
+	[TOKEN_STAR]= {NULL,binary,PREC_FACTOR},
+	[TOKEN_BANG]= {NULL,NULL,PREC_NONE},
+	[TOKEN_BANG_EQUAL]= {NULL,NULL,PREC_NONE},
+	[TOKEN_EQUAL]= {NULL,NULL,PREC_NONE},
+	[TOKEN_EQUAL_EQUAL]= {NULL,NULL,PREC_NONE},
+	[TOKEN_GREATER]= {NULL,NULL,PREC_NONE},
+	[TOKEN_GREATER_EQUAL] = {NULL,NULL,PREC_NONE},
+	[TOKEN_LESS]= {NULL,NULL,PREC_NONE},
+	[TOKEN_LESS_EQUAL]= {NULL,NULL,PREC_NONE},
+	[TOKEN_IDENTIFIER]= {NULL,NULL,PREC_NONE},
+	[TOKEN_STRING]= {NULL,NULL,PREC_NONE},
+	[TOKEN_NUMBER]= {number,NULL,PREC_NONE},
+	[TOKEN_AND]= {NULL,NULL,PREC_NONE},
+	[TOKEN_CLASS]= {NULL,NULL,PREC_NONE},
+	[TOKEN_ELSE]= {NULL,NULL,PREC_NONE},
+	[TOKEN_FALSE]= {NULL,NULL,PREC_NONE},
+	[TOKEN_FOR]= {NULL,NULL,PREC_NONE},
+	[TOKEN_DEF]= {NULL,NULL,PREC_NONE},
+	[TOKEN_IF]= {NULL,NULL,PREC_NONE},
+	[TOKEN_NULL]= {NULL,NULL,PREC_NONE},
+	[TOKEN_OR]= {NULL,NULL,PREC_NONE},
+	[TOKEN_PRINT]= {NULL,NULL,PREC_NONE},
+	[TOKEN_RETURN]= {NULL,NULL,PREC_NONE},
+	[TOKEN_BASE]= {NULL,NULL,PREC_NONE},
+	[TOKEN_THIS]= {NULL,NULL,PREC_NONE},
+	[TOKEN_TRUE]= {NULL,NULL,PREC_NONE},
+	[TOKEN_VAR]= {NULL,NULL,PREC_NONE},
+	[TOKEN_WHILE]= {NULL,NULL,PREC_NONE},
+	[TOKEN_ERROR]= {NULL,NULL,PREC_NONE},
+	[TOKEN_EOF]= {NULL,NULL,PREC_NONE},
+};
+
+static void parsePrecedence(Precedence precedence) {
+	advance();
+	ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+	if(prefixRule == NULL) {
+		error("Expect expression.");
+		return;
+	}
+	
+	prefixRule();
+	
+	while(precedence <= getRule(parser.current.type)->precedence) {
+		advance();
+		ParseFn infixRule = getRule(parser.previous.type)->infix;
+		infixRule();
+	}
+}
+
+static ParseRule* getRule(TokenType type) {
+	return &rules[type];
+}
+
+static void expression() {
+	parsePrecedence(PREC_ASSIGNMENT);
+}
+
+bool compile(const char* source, Chunk* chunk) {
+	initScanner(source);
+	compilingChunk = chunk;
+	
+	parser.hadError = false;
+	parser.panicMode = false;
+	advance();
+	expression();
+	consume(TOKEN_EOF, "Expect end of expression.");
+	endCompiler();
+	return !parser.hadError;
 }
--- Olive-bci/vm.c
+++ Olive-bci/vm.c
@@ -81,6 +81,19 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(const char* source) {
-	compile(source);
-	return INTERPRET_OK;
+	Chunk chunk;
+	initChunk(&chunk);
+	
+	if (!compile(source, &chunk)) {
+		freeChunk(&chunk);
+		return INTERPRET_COMPILE_ERROR;
+	}
+	
+	vm.chunk = &chunk;
+	vm.ip = vm.chunk->code;
+	
+	InterpretResult result = run();
+	
+	freeChunk(&chunk);
+	return result;
 }
