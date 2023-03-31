ce024468e66c1936044a5a18be991cddfbed16e9
oldmanclancy <wldfngrs@gmail.com>
2023-03-31 01:50:30 +0100

    housekeeping

--- Olive-bci/table.c
+++ Olive-bci/table.c
@@ -75,6 +75,7 @@ bool tableGet(Table* table, Key* key, Value* value) {
 	if (table->count == 0) return false;
 	
 	Entry* entry = findEntry(table->entries, table->capacity, key);
+	if (entry == NULL) return false;
 	if (IS_NULL(entry->key)) return false;
 	
 	*value = entry->value;
